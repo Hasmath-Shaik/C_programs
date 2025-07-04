21e6426e28234c5b5707acdb8214b070f052156a
Hasmath Shaik <shaik.hasmathsulthana2709@gmail.com>
2025-07-04 09:12:34 +0530

    Update sum_two_numbers.c

--- sum_two_numbers.c
+++ sum_two_numbers.c
@@ -1,19 +1,9 @@
 #include <stdio.h>
-
 int main() {
-    int num1, num2, sum; // Declare integer variables
-
-    printf("Enter the first integer: ");
-    scanf("%d", &num1); // Read the first integer
-
-    printf("Enter the second integer: ");
-    scanf("%d", &num2); // Read the second integer
-
-    // Calculate the sum
-    sum = num1 + num2;
-
-    // Display the sum
-    printf("The sum of %d and %d is %d\n", num1, num2, sum);
-
-    return 0; 
+    int a, b, sum;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+    sum = a + b;
+    printf("Sum: %d\n", sum);
+    return 0;
 }
